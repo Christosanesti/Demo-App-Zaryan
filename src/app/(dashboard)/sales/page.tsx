92e8c0656aaa6aaa995fa53687e12b71b2b08b66
Christosanesti <navophoto@protonmail.com>
2025-06-01 01:03:48 +0330

    Fix

--- src/app/(dashboard)/sales/page.tsx
+++ src/app/(dashboard)/sales/page.tsx
@@ -2,7 +2,11 @@ import { getCurrentUserWithDB } from "@/lib/auth-utils";
 import SalesClient from "./_components/SalesClient";
 
 export default async function SalesPage() {
-  const { userSettings } = await getCurrentUserWithDB();
+  const user = await getCurrentUserWithDB();
 
-  return <SalesClient userSettings={userSettings} />;
+  if (!user || !user.settings) {
+    return null;
+  }
+
+  return <SalesClient userSettings={user.settings} />;
 }
